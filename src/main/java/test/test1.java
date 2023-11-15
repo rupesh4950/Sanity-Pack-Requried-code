fb381e3172af7a694e8e3c2ae55d9664be0e34fe
Rupesh M <101011309+rupesh4950@users.noreply.github.com>
2023-11-15 15:41:21 +0530

    Update test1.java

--- src/main/java/test/test1.java
+++ src/main/java/test/test1.java
@@ -2,16 +2,15 @@ package test;
 
 
 
-//import org.testng.annotations.Test;
+import org.testng.annotations.Test;
 public class test1 {
-	
-//	@Test
+	@Test
+	public void run() {
+		System.out.println("runiing");
+	}
 	public static void main(String [] args){
 		test1 t=new test1();
 		t.run();
-	}
-	public void run() {
-		System.out.println("runiing");
 	}
 }
 
